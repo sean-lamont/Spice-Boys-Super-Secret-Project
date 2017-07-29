5499ac7b562e1018d3833929eac76b6c979e17fa
Noah <noahingham@gmail.com>
2017-07-29 19:42:30 +1000

    Moved to new Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,88 +1,35 @@
 import React, { Component } from 'react';
-import MapGL from 'react-map-gl';
-import {defaultMapStyle, dataLayer} from '../map-style.js';
-import {fromJS} from 'immutable';
-import {json as requestJson} from 'd3-request';
 
-class Map extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-            mapStyle: defaultMapStyle,
-			height: 400,
-			width: 400,
-            latitude: 40,
-            longitude: -100,
-            viewport: {
-                latitude: -35.3,
-                longitude: 149.1,
-                zoom: 10,
-                bearing: 0,
-                pitch: 0,
-            }
-		}
-	}
+/// / ES6
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
-    componentDidMount() {
-        window.addEventListener('resize', this._resize);
-        this._resize();
+// ES5
+//var ReactMapboxGl = require("react-mapbox-gl");
+//var Layer = ReactMapboxGl.Layer;
+//var Feature = ReactMapboxGl.Feature;
 
-        requestJson(document.location.href + '/data/ACT Division Boundaries.geojson', (error, response) => {
-            if (!error) {
-                this._loadData(response);
-            }
+
+export default class Map extends Component {
+
+    render() {
+        const Map = ReactMapboxGl({
+            accessToken: "pk.eyJ1IjoiYWNlb2ZzcGllcyIsImEiOiJjajVvaDg0d2Q0MXJhMnFvODUzNjQ3MmZ5In0.xLzbJqDG59zkyw2CKEXaow"
         });
-    }
 
-    _loadData = data => {
-
-        //updatePercentiles(data, f => f.properties.income[this.state.year]);
-        const mapStyle = defaultMapStyle
-        // Add geojson source to map
-            .setIn(['sources', 'suburbs'], fromJS({type: 'geojson', data}))
-            // Add point layer to map
-            .set('layers', defaultMapStyle.get('layers').push(dataLayer));
-
-        this.setState({data, mapStyle});
+        return <Map
+            style="mapbox://styles/aceofspies/cj5ow46yd0d132rmoxb04lwoc"
+            center={[149.1, -35.3]}
+            zoom={[10]}
+            containerStyle={{
+                height: "100vh",
+                width: "65vw"
+            }}>
+            <Layer
+                type="symbol"
+                id="marker"
+                layout={{"icon-image": "marker-15"}}>
+                <Feature coordinates={[-35.3, 149.1]}/>
+            </Layer>
+        </Map>
     };
-
-    updatePercentiles = (featureCollection, accessor) => {
-        const {features} = featureCollection;
-        const scale = scaleQuantile().domain(features.map(accessor)).range(range(9));
-        features.forEach(f => {
-            const value = accessor(f);
-            f.properties.value = value;
-            f.properties.percentile = scale(value);
-        });
-    };
-
-
-
-    _resize = () => {
-        this.setState({
-                width: this.refs.mapBox.clientWidth,
-                height: this.refs.mapBox.clientHeight
-        });
-    };
-
-  render() {
-      const {viewport, mapStyle} = this.state;
-    return (
-			<div ref="mapBox" className="map">
-				<MapGL
-                    {...viewport}
-					width={this.state.width}
-					height={this.state.height}
-					mapboxApiAccessToken={'pk.eyJ1IjoiYWNlb2ZzcGllcyIsImEiOiJjajVvaDg0d2Q0MXJhMnFvODUzNjQ3MmZ5In0.xLzbJqDG59zkyw2CKEXaow'}
-                    mapStyle={mapStyle}
-					onViewportChange={this.onViewportChange}
-					{...viewport}
-				/>
-			</div>
-    );
-  }
-
-	onViewportChange = viewport => this.setState({ viewport });
 }
-
-export default Map;
