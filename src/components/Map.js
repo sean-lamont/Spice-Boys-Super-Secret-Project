263c2a4900c6ee6ad47d5f16ffc65283f6e71d09
Jaz Gulati <jaskaran.g15@gmail.com>
2017-07-29 12:32:59 +1000

    Added react-map-gl

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import MapGL from 'react-map-gl';
 
 class Map extends Component {
   render() {
     return (
 			<div className="map">
-      	
+				<MapGL
+					width={400}
+					height={400}
+					latitude={37.7577}
+					longitude={-122.4376}
+					zoom={8}
+					onChangeViewport={viewport => {
+						// Call `setState` and use the state to update the map.
+					}}
+				/>
 			</div>
     );
   }
