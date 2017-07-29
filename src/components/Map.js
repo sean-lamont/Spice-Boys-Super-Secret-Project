308826aa83be6d61fee750a263e02cd591413750
Matt Brown <matthew.brown.dev@gmail.com>
2017-07-29 19:18:53 +1000

    Fix build...

--- src/components/Map.js
+++ src/components/Map.js
@@ -46,18 +46,6 @@ class Map extends Component {
         this.setState({data, mapStyle});
     };
 
-    updatePercentiles = (featureCollection, accessor) => {
-        const {features} = featureCollection;
-        const scale = scaleQuantile().domain(features.map(accessor)).range(range(9));
-        features.forEach(f => {
-            const value = accessor(f);
-            f.properties.value = value;
-            f.properties.percentile = scale(value);
-        });
-    };
-
-
-
     _resize = () => {
         this.setState({
                 width: this.refs.mapBox.clientWidth,
