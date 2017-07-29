48006405c9bd5b54c1f3fa0fd92d7a47787bcf60
Noah <noahingham@gmail.com>
2017-07-30 07:45:52 +1000

    Uses deck.gl

---
+++ src/components/Overlay.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {setParameters} from 'luma.gl';
+import DeckGL, {GeoJsonLayer} from 'deck.gl';
+
+const LIGHT_SETTINGS = {
+    lightsPosition: [-125, 50.5, 5000, -122.8, 48.5, 8000],
+    ambientRatio: 0.2,
+    diffuseRatio: 0.5,
+    specularRatio: 0.3,
+    lightsStrength: [1.0, 0.0, 2.0, 0.0],
+    numberOfLights: 2
+};
+
+export default class Overlay extends Component {
+
+    static get defaultViewport() {
+        return {
+            latitude: -35.3,
+            longitude: 149.1,
+            zoom: 10,
+            maxZoom: 16,
+            pitch: 45,
+            bearing: 0,
+            height: 500,
+            width: 500,
+            mapStyle: "mapbox://styles/mapbox/outdoors-v9",
+        };
+    }
+
+    _initialize(gl) {
+        setParameters(gl, {
+            depthTest: true,
+            depthFunc: gl.LEQUAL
+        });
+    }
+
+    render() {
+        const {viewport, data, colorScale} = this.props;
+
+        if (!data) {
+            return null;
+        }
+
+        const layer = new GeoJsonLayer({
+            id: 'geojson',
+            data,
+            opacity: 0.8,
+            stroked: false,
+            filled: true,
+            extruded: true,
+            wireframe: true,
+            fp64: true,
+            getElevation: f => f.properties.division_name.length * 100,
+            getFillColor: f => colorScale(f.properties.division_name.length/10),
+            getLineColor: f => [255, 255, 255],
+            lightSettings: LIGHT_SETTINGS,
+            pickable: Boolean(this.props.onHover),
+            onHover: this.props.onHover
+        });
+
+        return (
+            <DeckGL {...viewport}  layers={ [layer] } onWebGLInitialized={this._initialize} />
+        );
+    }
+}
