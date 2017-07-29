b27cac15acbb9e54c7e4b5e96ee34ef805fb24fe
Noah <noahingham@gmail.com>
2017-07-30 08:26:52 +1000

    Weird problems

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,73 @@
-import React, { Component } from 'react';
+/* global window,document */
+import React, {Component} from 'react';
+//import {render} from 'react-dom';
+import MapGL from 'react-map-gl';
+import Overlay from './Overlay.js';
+import Dimensions from 'react-dimensions'
 
-/// / ES6
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import {json as requestJson} from 'd3-request';
 
-export default class Map extends Component {
+// Set your mapbox token here
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiYWNlb2ZzcGllcyIsImEiOiJjajVvaDg0d2Q0MXJhMnFvODUzNjQ3MmZ5In0.xLzbJqDG59zkyw2CKEXaow'; // eslint-disable-line
+
+// Source data GeoJSON
+const DATA_URL = 'https://raw.githubusercontent.com/Spice-Boys/Spice-Boys-Super-Secret-Project/master/public/datasets/ACT%20Division%20Boundaries.geojson'; // eslint-disable-line
+
+const colorScale = r => [140, r * 255, 200 * (1 - r)];
+
+class Map extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                ...Overlay.defaultViewport,
+            },
+            data: null
+        };
+
+        requestJson(DATA_URL, (error, response) => {
+            if (!error) {
+                this.setState({data: response});
+            }
+        });
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this._resize.bind(this));
+        this._resize();
+    }
+
+    _resize() {
+        this._onViewportChange({
+            width: this.refs.mapBox.clientWidth,
+            height: this.refs.mapBox.clientHeight
+        });
+    }
+
+    _onViewportChange(viewport) {
+        this.setState({
+            viewport: {...this.state.viewport, ...viewport}
+        });
+    }
 
     render() {
-        const Map = ReactMapboxGl({
-            accessToken: "pk.eyJ1IjoiYWNlb2ZzcGllcyIsImEiOiJjajVvaDg0d2Q0MXJhMnFvODUzNjQ3MmZ5In0.xLzbJqDG59zkyw2CKEXaow"
-        });
-
-        return <Map
-            style="mapbox://styles/aceofspies/cj5ow46yd0d132rmoxb04lwoc"
-            center={[149.1, -35.3]}
-            zoom={[10]}
-            containerStyle={{
-                height: "100vh",
-                width: "65vw"
-            }}>
-            <Layer
-                type="symbol"
-                id="marker"
-                layout={{"icon-image": "marker-15"}}>
-                <Feature coordinates={[-35.3, 149.1]}/>
-            </Layer>
-        </Map>
-    };
+        const {viewport, data} = this.state;
+        return (
+            <div className="map" ref="mapBox">
+                <MapGL
+                    {...viewport}
+                    onViewportChange={this._onViewportChange.bind(this)}
+                    mapboxApiAccessToken={MAPBOX_TOKEN}
+                >
+                    <Overlay viewport={viewport}
+                                   data={data}
+                                   colorScale={colorScale}
+                                   />
+                </MapGL>
+            </div>
+        );
+    }
 }
+
+export default Map
